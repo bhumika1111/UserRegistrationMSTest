6d3f104ecd6194738053cce2abb9a45752f1a58c
bhumika1111 <bhoyarpatil@434>
2022-08-19 22:01:39 +0530

    *Check User Password For Rule Four Using MSTest *

---
+++ UserPassword.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace UserPasswordRule4
+{
+    public class UserPassword
+    {
+        public static string REGEX_PASSWORD = "^[A-Z]{1}[0-9]{1}['@'][A-Z a-z]{7}$";
+        public bool ValidatePasswordFour(string Password)
+        {
+            return Regex.IsMatch(Password, REGEX_PASSWORD);
+
+        }
+    }
+}
