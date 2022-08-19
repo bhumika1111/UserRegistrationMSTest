b7a57e6b42eea813a92dfc17ea5a7a1539e1488d
bhumika1111 <bhoyarpatil@434>
2022-08-19 20:08:46 +0530

    *Check User First Name Using MSTest Program File*

---
+++ UserFirstName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    public class UserFirstName
+    {
+        public static string REGEX_FIRSTNAME = "^[A-Z]{1}[a-z]{2,7}$";
+        public bool ValidateFirstName(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, REGEX_FIRSTNAME);
+
+        }
+    }
+}
