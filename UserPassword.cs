3ba7e542e8c5cdfdd8037e72a3931b3c6b14b0b4
bhumika1111 <bhoyarpatil@434>
2022-08-19 21:31:10 +0530

    *Check User Password For Rule Two Using MSTest*

---
+++ UserPassword.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserPasswordRule2
+{
+    public class UserPassword
+    {
+        public static string REGEX_PASSWORD = "^[A-Z]{1}[A-Z a-z]{7}$";
+        public bool ValidatePasswordTwo(string Password)
+        {
+            return Regex.IsMatch(Password, REGEX_PASSWORD);
+        }
+    }
+}
