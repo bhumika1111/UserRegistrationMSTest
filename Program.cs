f565a8391f6526e9bffb6549ccc706afdfcebc27
bhumika1111 <bhoyarpatil@434>
2022-08-19 20:03:35 +0530

    *Added User Registration Using MSTest Program File*

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("::::::Welcome To User Registration Program Using MSTesting:::::::::");
+            Console.WriteLine("---------------------------------------------------------------------");
+        }
+    }
+}
