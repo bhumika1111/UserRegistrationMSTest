449689f7106acaed73448fe837388a04b91321eb
bhumika1111 <bhoyarpatil@434>
2022-08-19 20:37:18 +0530

    *Check User Last Name Using MSTest*

---
+++ UserLastName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationMSTest
+{
+    public class UserLastName
+    {
+        public static string REGEX_LASTNAME = "^[A-Z]{1}[a-z]{2,7}$";
+        public bool ValidateLastName(string LastName)
+        {
+            return Regex.IsMatch(LastName, REGEX_LASTNAME);
+
+        }
+    }
+}
